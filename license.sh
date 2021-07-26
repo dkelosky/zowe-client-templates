12398edf2580d1e0d96267fb7b27e254c6a7c6e0
Dan Kelosky <dkelosky@gmail.com>
2021-07-26 11:10:23 -0400

    add license
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

---
+++ license.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Checking source for license files..."
+for i in packages/*/src/*.ts # or whatever other pattern...
+do
+  if ! grep -q Copyright $i
+  then
+    echo "Adding copyright notice to $i"
+    cat copyright.txt $i >$i.new && mv $i.new $i
+  fi
+done
